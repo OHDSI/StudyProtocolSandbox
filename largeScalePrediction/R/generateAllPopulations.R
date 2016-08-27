10df13058a727e6d7c330efc69407c34424cd99e
jreps <jreps@its.jnj.com>
2016-08-27 06:15:33 -0400

    edits

--- largeScalePrediction/R/generateAllPopulations.R
+++ largeScalePrediction/R/generateAllPopulations.R
@@ -2,6 +2,7 @@
 #
 # Copyright 2016 Observational Health Data Sciences and Informatics
 #
+# This file is part of LargeScalePrediction package
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
