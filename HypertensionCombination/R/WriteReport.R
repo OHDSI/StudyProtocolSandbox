1a25d09d8f2b9d31065f99b9bfc7e4f88a1e9ca5
jungsungjae <sungjae.2425@gmail.com>
2017-07-23 17:21:12 +0900

    add markdown report file

--- HypertensionCombination/R/WriteReport.R
+++ HypertensionCombination/R/WriteReport.R
@@ -1,5 +1,5 @@
 writeReport <- function(exportFolder, outputFile) {
-    rmarkdown::render(system.file("markdown", "Report.rmd", package = "HypertensionCombination"),
+    rmarkdown::render(system.file("markdown", "HTN_combi.rmd", package = "HypertensionCombination"),
                       params = list(exportFolder = exportFolder),
                       output_file = outputFile,
                       rmarkdown::word_document(toc = TRUE, fig_caption = TRUE))
