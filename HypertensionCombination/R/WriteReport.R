7d89bcba157979b3eadfd06f612b4f1a9026d240
jungsungjae <sungjae.2425@gmail.com>
2017-07-23 17:33:02 +0900

    edit markdown export type

--- HypertensionCombination/R/WriteReport.R
+++ HypertensionCombination/R/WriteReport.R
@@ -2,5 +2,5 @@ writeReport <- function(exportFolder, outputFile) {
     rmarkdown::render(system.file("markdown", "HTN_combi.rmd", package = "HypertensionCombination"),
                       params = list(exportFolder = exportFolder),
                       output_file = outputFile,
-                      rmarkdown::word_document(toc = TRUE, fig_caption = TRUE))
+                      rmarkdown::html_document(toc = TRUE, fig_caption = TRUE))
 }
