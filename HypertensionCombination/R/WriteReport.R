c8fae0f123873b277f5987f65adecd71eb90004f
jungsungjae <sungjae.2425@gmail.com>
2017-07-23 19:31:31 +0900

    update markdown file to test

--- HypertensionCombination/R/WriteReport.R
+++ HypertensionCombination/R/WriteReport.R
@@ -1,5 +1,4 @@
 writeReport <- function(exportFolder) {
-	print(exportFolder)
     rmarkdown::render(system.file("markdown", "HTN_combi.rmd", package = "HypertensionCombination"),
                       params = list(exportFolder = exportFolder),
                       output_file = file.path(exportFolder,"report.html"),
