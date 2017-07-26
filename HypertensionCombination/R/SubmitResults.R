8bfe01d6485fea785ca5c67d4c5a71fbc92537e6
SUNGJAE JUNG <sungjae.2425@gmail.com>
2017-07-26 22:11:42 +0900

    edit submit to google drive

--- HypertensionCombination/R/SubmitResults.R
+++ HypertensionCombination/R/SubmitResults.R
@@ -1,20 +1,32 @@
-submitResults <- function(exportFolder, from_addr, msg) {
+submitResults <- function(exportFolder, localName) {
   zipName <- file.path(exportFolder, "StudyResults.zip")
   if (!file.exists(zipName)) {
     stop(paste("Cannot find file", zipName))
   }
   writeLines(paste0("Uploading file '", zipName, "' to study coordinating center"))
   
-  file_attachment <- mime() %>%
-  to("sungjae.2425@gmail.com") %>%
-  from(from_addr) %>%
-#  text_body("This is testing") -> text_msg
-#  text_msg %>%
-  subject("Study result") %>%
-  html_body(paste0("<html><body>",msg,"</body></html>")) %>%
-  attach_file(zipName)
-  
-  result<-send_message(file_attachment)
+## Google Drive (googledrive)
+googledrive::drive_auth(new_user=T)
+
+mirrors <- drive_upload(media=zipName
+                        ,path=as_id("https://drive.google.com/drive/u/0/folders/0B4S3mMh259ntekVDQU9xSTlRSGs")
+                        ,name=paste0("OHDSI_HTN_combi_",localName,".zip")
+                        ,type=drive_mime_type("zip")
+)
+
+## Gmail (gmailr)
+#  file_attachment <- mime() %>%
+#  to("sungjae.2425@gmail.com") %>%
+#  from(from_addr) %>%
+##  text_body("This is testing") -> text_msg
+##  text_msg %>%
+#  subject("Study result") %>%
+#  html_body(paste0("<html><body>",msg,"</body></html>")) %>%
+#  attach_file(zipName)
+#  
+#  result<-send_message(file_attachment)
+
+## S3
 #  result <- OhdsiSharing::putS3File(file = zipName,
 #                                    bucket = "ohdsi-study-htncombi",
 #                                    key = key,
