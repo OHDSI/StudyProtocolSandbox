729e59b3862da45317e2944799afe440b3d9a602
schuemie <schuemie@ohdsi.org>
2018-01-25 07:14:53 -0500

    Adding study comparing denosumab to zoledronic acid

---
+++ DenosumabBoneMetastases/R/CreateStudyAnalysisDetails.R
@@ -0,0 +1,101 @@
+# Copyright 2018 Observational Health Data Sciences and Informatics
+#
+# This file is part of DenosumabBoneMetastases
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Create the analyses details
+#'
+#' @details
+#' This function creates files specifying the analyses that will be performed.
+#'
+#' @param workFolder        Name of local folder to place results; make sure to use forward slashes
+#'                            (/)
+#'
+#' @export
+createAnalysesDetails <- function(workFolder) {
+  covariateSettings <- FeatureExtraction::createCovariateSettings(useDemographicsGender = TRUE,
+                                                                  useDemographicsAgeGroup = TRUE,
+                                                                  useDemographicsIndexYear = TRUE,
+                                                                  useDemographicsIndexMonth = TRUE,
+                                                                  useConditionGroupEraLongTerm = TRUE,
+                                                                  useConditionGroupEraShortTerm = TRUE,
+                                                                  useDrugGroupEraLongTerm = TRUE,
+                                                                  useDrugGroupEraShortTerm = TRUE,
+                                                                  useCharlsonIndex = TRUE,
+                                                                  addDescendantsToExclude = TRUE)
+  
+  getDbCohortMethodDataArgs <- CohortMethod::createGetDbCohortMethodDataArgs(restrictToCommonPeriod = TRUE,
+                                                                             washoutPeriod = 365,
+                                                                             excludeDrugsFromCovariates = FALSE,
+                                                                             covariateSettings = covariateSettings)
+  
+  createStudyPopArgs <- CohortMethod::createCreateStudyPopulationArgs(removeDuplicateSubjects = "keep first",
+                                                                      removeSubjectsWithPriorOutcome = TRUE,
+                                                                      riskWindowStart = 0,
+                                                                      riskWindowEnd = round(40.5 * 30.5),
+                                                                      addExposureDaysToEnd = FALSE)
+  
+  control <- Cyclops::createControl(noiseLevel = "quiet", 
+                                    cvType = "auto", 
+                                    tolerance = 2e-07, 
+                                    cvRepetitions = 1, 
+                                    startingVariance = 0.01,
+                                    seed = 123)
+  createPsArgs <- CohortMethod::createCreatePsArgs(control = control)
+  
+  stratifyByPsArgs <- CohortMethod::createStratifyByPsArgs()
+  
+  fitOutcomeModelArgs <- CohortMethod::createFitOutcomeModelArgs(modelType = "cox",
+                                                                 stratified = TRUE,
+                                                                 useCovariates = FALSE)
+  
+  cmAnalysis1 <- CohortMethod::createCmAnalysis(analysisId = 1,
+                                                description = "ITT",
+                                                getDbCohortMethodDataArgs = getDbCohortMethodDataArgs,
+                                                createStudyPopArgs = createStudyPopArgs,
+                                                createPs = TRUE,
+                                                createPsArgs = createPsArgs,
+                                                stratifyByPs = TRUE,
+                                                stratifyByPsArgs = stratifyByPsArgs,
+                                                fitOutcomeModel = TRUE,
+                                                fitOutcomeModelArgs = fitOutcomeModelArgs)
+  
+  cmAnalysisList <- list(cmAnalysis1)
+  
+  CohortMethod::saveCmAnalysisList(cmAnalysisList, file.path(workFolder, "cmAnalysisList.json"))
+}
+
+createTcos <- function(outputFolder) {
+  targetOfInterestId <- 1 # Denosumab
+  comparatorOfInterestId <- 2 # Zoledronic acid
+  outcomeOfInterestId <- 3 # Skeletal events
+  excludedCovariateConceptIds <- c(1524674, 
+                                   967823, 
+                                   21600884, 
+                                   21601153, 
+                                   21601194, 
+                                   21601195, 
+                                   40222444, 
+                                   21601136)
+  
+  allControlsFile <- file.path(outputFolder, "AllControls.csv")
+  allControls <- read.csv(allControlsFile)
+  controlOutcomes <- allControls[allControls$type == "Outcome", ]
+  dcos <- CohortMethod::createDrugComparatorOutcomes(targetId = targetOfInterestId,
+                                                     comparatorId = comparatorOfInterestId,
+                                                     outcomeIds = c(outcomeOfInterestId, controlOutcomes$outcomeId),
+                                                     excludedCovariateConceptIds = excludedCovariateConceptIds) 
+  dcosList <- list(dcos)
+  return(dcosList)
+}
---
+++ DenosumabBoneMetastases/extras/CodeToRun.R
@@ -0,0 +1,48 @@
+library(DenosumabBoneMetastases)
+options(fftempdir = "c:/FFtemp")
+
+maxCores <- 32
+studyFolder <- "c:/DenosumabBoneMetastases"
+dbms <- "pdw"
+user <- NULL
+pw <- NULL
+server <- Sys.getenv("PDW_SERVER")
+port <- Sys.getenv("PDW_PORT")
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms,
+                                                                server = server,
+                                                                user = user,
+                                                                password = pw,
+                                                                port = port)
+
+cdmDatabaseSchema <- "cdm_optum_extended_dod_v654.dbo"
+cohortDatabaseSchema <- "scratch.dbo"
+cohortTable <- "mschuemi_denosumab_optum"
+oracleTempSchema <- NULL
+outputFolder <- file.path(studyFolder, "optum")
+
+
+mailSettings <- list(from = Sys.getenv("mailAddress"),
+                     to = c(Sys.getenv("mailAddress")),
+                     smtp = list(host.name = "smtp.gmail.com", port = 465,
+                                 user.name = Sys.getenv("mailAddress"),
+                                 passwd = Sys.getenv("mailPassword"), ssl = TRUE),
+                     authenticate = TRUE,
+                     send = TRUE)
+
+result <- OhdsiRTools::runAndNotify({
+  execute(connectionDetails = connectionDetails,
+          cdmDatabaseSchema = cdmDatabaseSchema,
+          cohortDatabaseSchema = cohortDatabaseSchema,
+          cohortTable = cohortTable,
+          oracleTempSchema = oracleTempSchema,
+          outputFolder = outputFolder,
+          createCohorts = TRUE,
+          synthesizePositiveControls = TRUE,
+          runAnalyses = TRUE,
+          runDiagnostics = TRUE,
+          maxCores = 30)
+}, mailSettings = mailSettings, label = "denosumab")
+
+# combineAcrossDbs(folders = c("Mdcd", "Mdcr", "Optum", "Ccae"),
+#                  labels = c("MDCD", "MDCR", "Optum", "CCAE"),
+#                  outputFolder = "r:/Loop")
