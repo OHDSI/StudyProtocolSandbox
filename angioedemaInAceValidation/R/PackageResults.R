f14934ea79c8f754cca489d1c37240fe0c625c9a
jreps <jreps@its.jnj.com>
2018-10-09 16:00:18 -0400

    grep('inst/plp_models', folders)
    grep('inst/plp_models', folders) causing issues when empty

--- angioedemaInAceValidation/R/PackageResults.R
+++ angioedemaInAceValidation/R/PackageResults.R
@@ -33,7 +33,9 @@ packageResults <- function(outputFolder,
   # for each analysis copy the requested files...
   folders <- list.dirs(path = outputFolder, recursive = T, full.names = F)
   folders <- folders[grep('Analysis_', folders)]
-  folders <- folders[-grep('inst/plp_models', folders)] #in case using package directory
+  if(length(grep("inst/plp_models", folders))>0){  
+    folders <- folders[-grep('inst/plp_models', folders)] #in case using package directory
+    }
 
   #create export subfolder in workFolder
   exportFolder <- file.path(outputFolder, "export")
