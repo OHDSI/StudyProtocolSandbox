e0cdb363bcff74005efb4fbe61b2a2e6d804db4a
jreps <jreps@its.jnj.com>
2018-10-08 21:11:07 -0400

    added angioedema in ace inhibitor model validation
    A package to externally validate a model that has been developed to
    predict angioedema in new users of ace inhibitors since 2015

---
+++ angioedemaInAceValidation/R/main.R
@@ -0,0 +1,58 @@
+#' @export
+execute <- function(connectionDetails,
+                    databaseName,
+                    cdmDatabaseSchema,
+                    cohortDatabaseSchema,
+                    cohortTable,
+                    outputFolder,
+                    createCohorts = T,
+                    runValidation = T,
+                    packageResults = T,
+                    minCellCount = 5,
+                    sampleSize=NULL){
+
+  if (!file.exists(outputFolder))
+    dir.create(outputFolder, recursive = TRUE)
+
+  OhdsiRTools::addDefaultFileLogger(file.path(outputFolder, "log.txt"))
+
+  if(createCohorts){
+    OhdsiRTools::logInfo("Creating Cohorts")
+    createCohorts(connectionDetails,
+                  cdmDatabaseSchema=cdmDatabaseSchema,
+                  cohortDatabaseSchema=cohortDatabaseSchema,
+                  cohortTable=cohortTable,
+                  outputFolder = outputFolder)
+  }
+
+  if(runValidation){
+    OhdsiRTools::logInfo("Validating Models")
+    # for each model externally validate
+    analysesLocation <- system.file("plp_models",
+                                    package = "angioedemaInAceValidation")
+    val <- PatientLevelPrediction::evaluateMultiplePlp(analysesLocation = analysesLocation,
+                                                       outputLocation = outputFolder,
+                                                       connectionDetails = connectionDetails,
+                                                       validationSchemaTarget = cohortDatabaseSchema,
+                                                       validationSchemaOutcome = cohortDatabaseSchema,
+                                                       validationSchemaCdm = cdmDatabaseSchema,
+                                                       databaseNames = databaseName,
+                                                       validationTableTarget = cohortTable,
+                                                       validationTableOutcome = cohortTable,
+                                                       sampleSize = sampleSize)
+  }
+
+  # package the results: this creates a compressed file with sensitive details removed - ready to be reviewed and then
+  # submitted to the network study manager
+
+  # results saved to outputFolder/databaseName
+  if (packageResults) {
+    OhdsiRTools::logInfo("Packaging results")
+    packageResults(outputFolder = outputFolder,
+                   minCellCount = minCellCount)
+  }
+
+
+  invisible(NULL)
+
+}
